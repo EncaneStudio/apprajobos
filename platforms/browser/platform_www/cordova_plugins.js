57f148109c07c1508ed6a0d125495122ce87ed92
Zadan <jmonzo90@gmail.com>
2016-07-26 17:20:36 +0200

    sobre mi
    ADSADSAD

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -7,12 +7,27 @@ module.exports = [
         "clobbers": [
             "MusicControls"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-battery-status/www/battery.js",
+        "id": "cordova-plugin-battery-status.battery",
+        "pluginId": "cordova-plugin-battery-status",
+        "clobbers": [
+            "navigator.battery"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-battery-status/src/browser/BatteryProxy.js",
+        "id": "cordova-plugin-battery-status.Battery",
+        "pluginId": "cordova-plugin-battery-status",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-music-controls": "1.3"
+    "cordova-plugin-music-controls": "1.3",
+    "cordova-plugin-battery-status": "1.1.2"
 }
 // BOTTOM OF METADATA
 });
