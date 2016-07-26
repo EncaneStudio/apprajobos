6c8b2f2a4ede930b2b3d3aef0c07a7e9958ddaed
Xteal <kevinra8@gmail.com>
2016-07-26 16:23:58 +0200

    Platform android añadido

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,18 +1,41 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-music-controls/www/MusicControls.js",
-        "id": "cordova-plugin-music-controls.MusicControls",
-        "pluginId": "cordova-plugin-music-controls",
+        "file": "plugins/cordova-plugin-battery-status/www/battery.js",
+        "id": "cordova-plugin-battery-status.battery",
+        "pluginId": "cordova-plugin-battery-status",
         "clobbers": [
-            "MusicControls"
+            "navigator.battery"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-battery-status/src/browser/BatteryProxy.js",
+        "id": "cordova-plugin-battery-status.Battery",
+        "pluginId": "cordova-plugin-battery-status",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-nowplaying/www/NowPlaying.js",
+        "id": "cordova-plugin-nowplaying.NowPlaying",
+        "pluginId": "cordova-plugin-nowplaying",
+        "clobbers": [
+            "window.NowPlaying"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-nowplaying/src/browser/MNowPlaying.js",
+        "id": "cordova-plugin-nowplaying.MNowPlaying",
+        "pluginId": "cordova-plugin-nowplaying",
+        "clobbers": [
+            "window.NowPlaying"
         ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-music-controls": "1.3"
+    "cordova-plugin-battery-status": "1.1.2",
+    "cordova-plugin-nowplaying": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
